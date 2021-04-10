c48408aa26224608f55f15bf911e36358c8435ee
AliQasimzade <qasimzadeali4@gmail.com>
2021-04-10 19:00:00 +0400

    Changed position property in navigation

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-let container = document.querySelector('body');
-let head = document.querySelector('.head');
+let container = document.querySelector("body");
+let head = document.querySelector(".head");
 window.onscroll = function () {
-	myFunction();
+  myFunction();
 };
 
 function myFunction() {
-	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-		document.querySelector('.nav').style.boxShadow = '0 0 10px -5px #00000070';
-		document.querySelector('.nav').style.background = '#f8f9fa';
-	} else {
-		document.querySelector('.nav').style.boxShadow = 'none';
-		document.querySelector('.nav').style.background = 'none';
-	}
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector(".nav").classList.add("active");
+  } else {
+    document.querySelector(".nav").classList.remove("active");
+  }
 }
