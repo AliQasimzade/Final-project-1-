c07c8ec220c8995481701e422377e3205d4cfea4
Ali Gasimzadeh <aligasimzadeh9@gmail.com>
2021-04-10 16:29:31 +0400

    Created and added sections

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ window.onscroll = function () {
 function myFunction() {
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
 		document.querySelector('.nav').style.boxShadow = '0 0 10px -5px #00000070';
-		document.querySelector('.nav').style.background = '#edf6f9';
+		document.querySelector('.nav').style.background = '#f8f9fa';
 	} else {
 		document.querySelector('.nav').style.boxShadow = 'none';
 		document.querySelector('.nav').style.background = 'none';
