c7c5c47a82915d2e15729f994cce3f687aa4f174
AliQasimzade <qasimzadeali4@gmail.com>
2021-04-13 08:36:04 +0400

    Changed classNames

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let container = document.querySelector("body");
 let head = document.querySelector(".head");
-let hamburgerButton = document.querySelector(".hamburger");
+let hamburgerButton = document.querySelector(".hamburger-button");
 
 window.onscroll = function () {
   myFunction();
 };
 
 function myFunction() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 10) {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.querySelector(".nav").classList.add("active");
   } else {
     document.querySelector(".nav").classList.remove("active");
@@ -16,13 +16,11 @@ function myFunction() {
 let flag = true;
 
 hamburgerButton.addEventListener("click", () => {
-  if(flag){
-    document.querySelector(".links").classList.add("active")
-   
-    flag = false
-  }else{
-    document.querySelector(".links").classList.remove("active")
-    
-    flag = true
+  if (flag) {
+    document.querySelector(".links").classList.add("active");
+    flag = false;
+  } else {
+    document.querySelector(".links").classList.remove("active");
+    flag = true;
   }
-})
+});
