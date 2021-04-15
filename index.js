6d4697c5f14462d504712e8d73118b993eb90c79
AliQasimzade <qasimzadeali4@gmail.com>
2021-04-15 18:47:41 +0400

    fixed

--- index.js
+++ index.js
@@ -1,103 +1,96 @@
-let container = document.querySelector('body');
-let hamburgerButton = document.querySelector('.hamburger-button');
-let arrowLeft = document.querySelector('.arrow-left');
-let arrowLeftPath = document.querySelector('.arrow-left-path');
-let arrowRight = document.querySelector('.arrow-right');
-let arrowRightPath = document.querySelector('.arrow-right-path');
-let dots = Array.from(document.querySelectorAll('.dot'));
-let slideEl = document.querySelectorAll('.slider');
-let sliderContainer = document.querySelector('.slider-container');
+let container = document.querySelector("body");
+let hamburgerButton = document.querySelector(".hamburger-button");
+let arrowLeft = document.querySelector(".arrow-left");
+let arrowLeftPath = document.querySelector(".arrow-left-path");
+let arrowRight = document.querySelector(".arrow-right");
+let arrowRightPath = document.querySelector(".arrow-right-path");
+let dots = Array.from(document.querySelectorAll(".dot"));
+let slideEl = document.querySelectorAll(".slider");
+let sliderContainer = document.querySelector(".slider-container");
 let activeCount = 0;
 let slideNumber = dots.length;
 let flag = true;
 
 window.onscroll = function () {
-	myFunction();
+  myFunction();
 };
 
 function myFunction() {
-	if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-		document.querySelector('body header').classList.add('active');
-	} else {
-		document.querySelector('body header').classList.remove('active');
-	}
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    document.querySelector("body header").classList.add("active");
+  } else {
+    document.querySelector("body header").classList.remove("active");
+  }
 }
 
-hamburgerButton.addEventListener('click', () => {
-	if (flag) {
-		document.querySelector('body nav').classList.add('active');
-		flag = false;
-	} else {
-		document.querySelector('body nav').classList.remove('active');
-		flag = true;
-	}
+hamburgerButton.addEventListener("click", () => {
+  if (flag) {
+    document.querySelector("body nav").classList.add("active");
+    flag = false;
+  } else {
+    document.querySelector("body nav").classList.remove("active");
+    flag = true;
+  }
 });
 
-arrowLeft.addEventListener('click', () => {
-	arrowLeftPath.style.opacity = '0.3';
-	if (activeCount == 0) {
-		activeCount = 3;
-		getDotId(activeCount);
-		sliderContainer.style.transform = 'translateX(-75%)';
-		sliderContainer.style.transition = 'none';
-	} else {
-		let how = -(activeCount - 1) * 25;
-		sliderContainer.style.transform = `translateX(${how}%)`;
-		activeCount--;
-		getDotId(activeCount);
-		sliderContainer.style.transition = 'all 0.5s ease-in-out';
-	}
-	setTimeout(() => {
-		arrowLeftPath.style.opacity = '1';
-	}, 400);
+arrowLeft.addEventListener("click", () => {
+  arrowLeftPath.style.opacity = "0.3";
+  if (activeCount == 0) {
+    activeCount = 3;
+    getDotId(activeCount);
+    sliderContainer.style.transform = "translateX(-75%)";
+    sliderContainer.style.transition = "none";
+  } else {
+    let how = -(activeCount - 1) * 25;
+    sliderContainer.style.transform = `translateX(${how}%)`;
+    activeCount--;
+    getDotId(activeCount);
+    sliderContainer.style.transition = "all 0.5s ease-in-out";
+  }
+  console.log(activeCount);
+  setTimeout(() => {
+    arrowLeftPath.style.opacity = "1";
+  }, 400);
 });
 
-arrowRight.addEventListener('click', () => {
-	arrowRightPath.style.opacity = '0.3';
-	if (activeCount < slideNumber - 1) {
-		let how = -(activeCount + 1) * 25;
-		sliderContainer.style.transform = `translateX(${how}%)`;
-		activeCount++;
-		getDotId(activeCount);
-		sliderContainer.style.transition = 'all 0.5s ease-in-out';
-	} else {
-		activeCount = 0;
-		getDotId(activeCount);
-		sliderContainer.style.transform = 'translateX(0)';
-		sliderContainer.style.transition = 'none';
-	}
-	setTimeout(() => {
-		arrowRightPath.style.opacity = '1';
-	}, 400);
+arrowRight.addEventListener("click", () => {
+  arrowRightPath.style.opacity = "0.3";
+  if (activeCount < slideNumber - 1) {
+    let how = -(activeCount + 1) * 25;
+    sliderContainer.style.transform = `translateX(${how}%)`;
+    activeCount++;
+    getDotId(activeCount);
+    sliderContainer.style.transition = "all 0.5s ease-in-out";
+  } else {
+    activeCount = 0;
+    getDotId(activeCount);
+    sliderContainer.style.transform = "translateX(0)";
+    sliderContainer.style.transition = "none";
+  }
+  console.log(activeCount);
+
+  setTimeout(() => {
+    arrowRightPath.style.opacity = "1";
+  }, 400);
 });
 const getDotId = (key) => {
-	if (key == 0) {
-		dots[0].children[0].classList.add('active');
-		dots[1].children[0].classList.remove('active');
-		dots[2].children[0].classList.remove('active');
-		dots[3].children[0].classList.remove('active');
-	} else if (key == 1) {
-		dots[0].children[0].classList.remove('active');
-		dots[1].children[0].classList.add('active');
-		dots[2].children[0].classList.remove('active');
-		dots[3].children[0].classList.remove('active');
-	} else if (key == 2) {
-		dots[0].children[0].classList.remove('active');
-		dots[1].children[0].classList.remove('active');
-		dots[2].children[0].classList.add('active');
-		dots[3].children[0].classList.remove('active');
-	} else if (key == 3) {
-		dots[0].children[0].classList.remove('active');
-		dots[1].children[0].classList.remove('active');
-		dots[2].children[0].classList.remove('active');
-		dots[3].children[0].classList.add('active');
-	}
+    dots[0].children[0].classList.remove("active");
+    dots[1].children[0].classList.remove("active");
+    dots[2].children[0].classList.remove("active");
+    dots[3].children[0].classList.remove("active");
+	dots[key].chidren[0].classList.add("active")
 };
 
 const changeSlideAnimation = (key) => {
-	const diff = key - slideNumber;
-	const x = -slideNumber * 25 - diff * 25;
-	sliderContainer.style.transform = `translateX(${x}%)`;
-	sliderContainer.style.transition = 'all 0.5s ease-in-out';
-	getDotId(key);
+  const diff = key - slideNumber;
+  console.log(diff);
+
+  const x = -slideNumber * 25 - diff * 25;
+  console.log(x);
+  sliderContainer.style.transform = `translateX(${x}%)`;
+ activeCount = key
+  getDotId(key);
 };
