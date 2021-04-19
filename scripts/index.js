4fb41b58a808be065c9d65eb2f476b7640474080
Ali Gasimzadeh <aligasimzadeh9@gmail.com>
2021-04-19 10:01:33 +0400

    Added animations for Trafalgar

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ let apps = document.querySelector('.apps');
 let testimonials = document.querySelector('.testimonials');
 let aboutUs = document.querySelector('.about-us');
 let ourServices = document.querySelector('.our-services');
-let sectionTwoWrapper = document.querySelector('.section-2-wrapper');
+let sectionThreeWrapper = document.querySelector('.section-three-wrapper');
 let cardsWrapper = document.querySelector('.cards-wrapper');
 let circle = document.querySelector('.circle');
 
@@ -38,7 +38,7 @@ apps.addEventListener('click', (event) => {
 	event.preventDefault();
 	hamburgerButton.classList.remove('active');
 	nav.classList.remove('active');
-	sectionTwoWrapper.scrollIntoView({ behavior: 'smooth', block: 'center' });
+	sectionThreeWrapper.scrollIntoView({ behavior: 'smooth', block: 'center' });
 });
 
 testimonials.addEventListener('click', (event) => {
--- scripts/scrollAnimations.js
+++ scripts/scrollAnimations.js
@@ -1,10 +1,16 @@
-const target = document.querySelector('body ul');
+const ul = document.querySelector('body ul');
 const healthInfo = document.querySelector('.health-info');
 const healthImage = document.querySelector('.health-image');
 const servicesHeader = document.querySelector('.services-header');
 const servicesCards = document.querySelector('.services-cards');
+const sectionOneWrapperImg = document.querySelector('.section-one-wrapper img');
+const sectionOne = document.querySelector(".section-one");
+const sectionThreeimg = document.querySelector('.section-three-image');
+const sectionThree = document.querySelector(".section-three");
+const cardsArticle = document.querySelector('.cards-article');
+const cardsContainer = document.querySelector('.cards-container');
 
-function handleIntersection(entries) {
+const handleIntersection = (entries) => {
 	entries.map((entry) => {
 		if (entry.isIntersecting) {
 			entry.target.classList.add('visible');
@@ -13,9 +19,15 @@ function handleIntersection(entries) {
 }
 
 const observer = new IntersectionObserver(handleIntersection);
-const observer2 = new IntersectionObserver(handleIntersection);
-observer.observe(target);
+
+observer.observe(ul);
 observer.observe(healthInfo);
 observer.observe(healthImage);
 observer.observe(servicesHeader);
-observer2.observe(servicesCards);
+observer.observe(servicesCards);
+observer.observe(sectionOneWrapperImg);
+observer.observe(sectionOne);
+observer.observe(sectionThree);
+observer.observe(sectionThreeimg);
+observer.observe(cardsArticle);
+observer.observe(cardsContainer);
